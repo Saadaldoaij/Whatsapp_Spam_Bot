891baad00c1f4efc27be7c265ea45cc5dcc1796a
Saad Aldoaij <54961548+Saadaldoaij@users.noreply.github.com>
2022-06-23 19:53:25 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+#CMD----> pip install PyAutoGUI
+import pyautogui as gui
+import time
+
+
+
+gui.alert("Once you Enter OK the spam will starts! ") 
+while True:
+    gui.typewrite("THIS IS A SPAM MESSAGE ! ")
+    gui.press("enter")
+    
+                                                                     
+
+     
+          
+               
+                    
+                         
+                              
+                                   
+                                        
+                                             
+                                                  
+                                                       
+                                                            
+                                                                 
+                                                                      
+                                                                           
+                                                                                
+
+               
+                    
+                         
+                              
+                                   
